e16815d113a8a57e54e0113bb86734077338072c
Mikhail Kamarouski <Mikhail.Kamarouski@1pt.com>
2020-04-21 12:58:12 +0300

    Authorisation attempt consumption added

--- model/authorization/RemoteProcotoredSessionContext.php
+++ model/authorization/RemoteProcotoredSessionContext.php
@@ -23,6 +23,7 @@ declare(strict_types=1);
 namespace oat\remoteProctoring\model\authorization;
 
 use oat\oatbox\session\SessionContext;
+use oat\tao\model\security\SecurityException;
 
 /**
  * Authorization Provider that verifies if the tes
@@ -31,6 +32,9 @@ class RemoteProcotoredSessionContext implements SessionContext
 {
     /** @var string */
     private $deliveryExecutionId;
+
+    /**  @var bool */
+    private $consumed = false;
 
     public function __construct(string $deliveryExecutionId)
     {
@@ -43,5 +47,21 @@ class RemoteProcotoredSessionContext implements SessionContext
     public function getDeliveryExecutionId(): string
     {
         return $this->deliveryExecutionId;
+    }
+
+    /**
+     * @throws SecurityException
+     */
+    public function consume(): void
+    {
+        if ($this->consumed) {
+            throw new SecurityException('Proctorio authorisation attempt has been already consumed');
+        }
+        $this->consumed = true;
+    }
+
+    public function isConsumed(): bool
+    {
+        return $this->consumed;
     }
 }

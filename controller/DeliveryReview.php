12408c15461e8aa061ede0720da77d2a392df920
mike-ionut-mihai-sandu-tao <ionut-mihai@taotesting.com>
2020-04-23 14:42:39 +0200

    removed \n

--- controller/DeliveryReview.php
+++ controller/DeliveryReview.php
@@ -45,7 +45,6 @@ class DeliveryReview extends Controller implements ServiceLocatorAwareInterface
             $deliveryId = tao_helpers_Uri::decode($requestBody['uri']);
             $reviewUrl = $this->getProctorioApiService()->findReviewUrl($deliveryId);
 
-
             $response = [
                 'data' => ['url' => $reviewUrl],
                 'success' => true
